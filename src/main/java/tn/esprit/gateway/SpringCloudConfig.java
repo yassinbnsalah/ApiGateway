500d764904a14112efa606e7d65115dabc629e5d
yassinebnsalah <yacinbnsalh@gmail.com>
2023-12-12 20:05:46 +0100

    FV

--- src/main/java/tn/esprit/gateway/SpringCloudConfig.java
+++ src/main/java/tn/esprit/gateway/SpringCloudConfig.java
@@ -4,18 +4,20 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@CrossOrigin(origins="*")
 public class SpringCloudConfig {
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         System.out.println("hello");
         return builder.routes()
 
-                .route("CondidatMicroService", r -> r.path("/candidats/**")
-                        .uri("http://localhost:8088/"))
-                .route("JobMicroService", r -> r.path("/jobs/**")
-                        .uri("http://job_A2:8091/"))
+                .route("CandidatApiApplication", r -> r.path("/candidats/**")
+                        .uri("http://candidat:8082/"))
+                /*.route("JobMicroService", r -> r.path("/jobs/**")
+                        .uri("http://localhost:8081/"))*/
                 .build();
     }
 }
